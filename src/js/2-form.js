64bcb2270c377f9a0307ac5cd2b6ca150645b4d9
Pakjetoshka <frostcraftnumber1@gmail.com>
2024-11-17 01:36:55 +0200

    17.11.24 - hw9 - first_comit

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,25 @@
-console.log("form");
+const form = document.querySelector('.feedback-form');
+const localStorageMassage = "message-example";
+const localStorageEmail = "email-example";
+
+form.message.value = localStorage.getItem(localStorageMassage) ?? "";
+form.email.value = localStorage.getItem(localStorageEmail) ?? "";
+
+form.addEventListener("input",() => {
+    localStorage.setItem(localStorageMassage, form.message.value);
+    localStorage.setItem(localStorageEmail, form.email.value);
+});
+
+
+form.addEventListener("submit", (evt) => {
+    if(form.message.value === "" || form.email.value === "")
+    {
+        return alert("Fill please all fields");
+    }
+    else{
+        evt.preventDefault();
+        localStorage.removeItem(localStorageMassage);
+        localStorage.removeItem(localStorageEmail);
+        form.reset();
+    }
+});
